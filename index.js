062d8adaae3a4a35d130cf080a8aaf3224690837
Coder-AQ <166115761+Coder-AQ@users.noreply.github.com>
2024-05-01 00:27:36 -0700

    AQ-simple calculator
    made by Aniqa

---
+++ index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+//SHABANG      "shabang" : unknown word.
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstnumber" },
+    { message: "enter second number", type: "number", name: "secondnumber" },
+    { message: "select one of the operator",
+        type: "list",
+        name: "operator",
+        choices: ["ADDITION", "SUBTRACTION", "DIVISION", "MULTIPLICATION"] },
+]);
+if (answer.operator === "ADDITION") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "SUBTRACTION") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "DIVISION") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else if (answer.operator === "MULTIPLICATION") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else {
+    console.log("please select valid operator");
+}
